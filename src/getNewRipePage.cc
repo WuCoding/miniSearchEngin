5d23c69ddf3a2f8d2e0424974cfde2a78eb8c8ba
WuCoding <wcy_best@outlook.com>
2019-12-13 14:05:08 +0800

    ...

---
+++ src/getNewRipePage.cc
@@ -0,0 +1,60 @@
+#include "../inc/func.h"
+
+void print(XMLElement* elem,string elemName){
+	const char* txt;
+	if(elem==nullptr){
+		cout<<elemName<<" is null"<<endl;
+	}else{
+		txt=elem->GetText();
+		if(txt==nullptr){
+			cout<<elemName<<" :null"<<endl;
+		}else{
+			cout<<elemName<<": "<<txt<<endl;
+		}
+	}
+}
+
+void test(string fileName){
+	XMLDocument doc;
+	XMLError errXml=doc.LoadFile(fileName.c_str());
+	if(XML_SUCCESS==errXml){
+		XMLElement* Rss=doc.RootElement();//rss
+		XMLElement* Channel=Rss->FirstChildElement("channel");//channel
+
+		XMLElement* Title=Channel->FirstChildElement("title");//title
+		XMLElement* Link=Channel->FirstChildElement("link");//link
+		XMLElement* Description=Channel->FirstChildElement("description");//description
+		XMLElement* Content=Channel->FirstChildElement("content");//content
+		
+		print(Title,"title");
+		print(Link,"link");
+		print(Description,"description");
+		print(Content,"content");
+
+		string s;
+		XMLElement* Item=Channel->FirstChildElement("item");//item
+		while(Item){
+			Title=Item->FirstChildElement("title");
+			Link=Item->FirstChildElement("link");
+			Description=Item->FirstChildElement("description");
+			Content=Item->FirstChildElement("content");
+
+			cin>>s;
+			print(Title,"title");
+			cin>>s;
+			print(Link,"link");
+			cin>>s;
+			print(Description,"description");
+			cin>>s;
+			print(Content,"content");
+
+			Item=Item->NextSiblingElement("item");
+		}
+	}
+}
+
+int main(){
+	string fileName;
+	cin>>fileName;
+	test(fileName);
+}
